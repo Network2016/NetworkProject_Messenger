dc2965e3a742f12311ada41ceebc45fb4d35f993
lunahc <lunahc92@gmila.com>
2016-06-09 11:36:10 +0900

    initial commit

--- NetworkProject_Messenger/server.cpp
+++ NetworkProject_Messenger/server.cpp
@@ -18,27 +18,20 @@
 #define GETCMD "download"
 #define QUITCMD "quit"
 
-
-
 int room_size;
-
-
-
 int portnum = 0;
 
 
 
+//
+//
+//
 int main(int argc, char *argv[]){
-    List* userList = createList();
-    
-    
     
     struct sockaddr_in server;
     int new_sock, server_sock = -1;
     int addrlen;
-    char buf[102400], command[128], file[128], port[128], ip[128];
-    struct hostent *hostp;
-    
+    char command[128], buf[128];
     
     if (argc != 2) {
         (void) fprintf(stderr,"usage: %s port# \n",argv[0]);
@@ -73,6 +66,7 @@ int main(int argc, char *argv[]){
     //
     //
     while(1){
+        printf("waiting...\n");
         // Input from the keyboard
         memset(command, 0x00, sizeof command);
         
@@ -93,37 +87,52 @@ int main(int argc, char *argv[]){
         printf("%s", command);
         
         
+        //	for test
+//        if (write(new_sock, "connection ok", strlen("connection ok")) < 0) {
+//            perror("write");
+//            exit(1);
+//        }
+//        close(new_sock);
+//        printf("after write\n");
         
         
         if(!strncmp(command, "@quit", 5)){			// quit
         }
         else if(!strncmp(command, "@exit", 5)){		// exit
+            current = head;
+            memset(buf, 0x00, sizeof(buf));
+            while(current != NULL){
+                strcat(buf, current->ID);
+                strcat(buf, current->IP);
+                strcat(buf, toArray(current->port));
+                strcat(buf, toArray(current->room));
+                strcat(buf, "\n");
+                
+                if (write(new_sock, buf, strlen(buf)) < 0) {
+                    perror("write");
+                    exit(1);
+                }
+            }
         }
         else if(!strncmp(command, "@connect", 8)){	// connect
-			printf("in connect\n");
-            Data user;
-            strcpy(user.IP, inet_ntoa(server.sin_addr));
-            strcpy(user.ID, &command[10]);
-            user.port = ntohs(server.sin_port);
+            printf("in connection...\n");
             
-            insert(userList, user);
+            insertFirst(inet_ntoa(server.sin_addr), ntohs(server.sin_port), &command[10], 0);
             
-            if (write(new_sock, "connection ok", strlen("connecting success!")) < 0) {
+            if (write(new_sock, "connection ok", strlen("connection ok")) < 0) {
                 perror("write");
                 exit(1);
             }
         }
         else if(!strncmp(command, "@getlist", 8)){		// user & room list
-            List* list_ptr = userList;
-            //while(list_ptr != null)
-                printf("%s %s %d", list_ptr->data.IP, list_ptr->data.IP, list_ptr->data.port);
-            
-            
-//            printf("ID : %s, IP : %s, Port# : %d", user.id, user.ip, user.port); 			// need to fix
+            getList(buf);
+            if (write(new_sock, buf, strlen(buf)) < 0) {
+                perror("write");
+                exit(1);
+            }
         }
         else if(!strncmp(command, "@invite", 6)){	// exit
-            close(server_sock);
-            return 0;
+            // invite function
         }
         else if(!strncmp(command, "@mkroom", 6)){	// mkroom
             room_size++;
@@ -131,408 +140,5 @@ int main(int argc, char *argv[]){
         else{										// error
             
         }
-        
-        printf("pass\n");
     }
 } /* main - hw2.c */
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        else {										// download
-//            int i, j;
-//            
-//            for(i=9; command[i] != ' '; i++)	// 9 = initial ip location
-//                ip[i-9] = command[i];
-//            
-//            for(j=0; command[++i] != ' '; j++)	// port
-//                port[j] = command[i];
-//            
-//            for(j=0; command[++i] != '\n'; j++)	// file
-//                file[j] = command[i];
-//            
-//            
-//            if ((request_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-//                perror("socket");
-//                exit(1);
-//            }
-//            
-//            if ((hostp = gethostbyname(ip)) == 0) {
-//                fprintf(stderr,"%s: unknown host\n", ip);
-//                exit(1);
-//            }
-//            
-//            
-//            memset((void *) &request, 0, sizeof request);
-//            request.sin_family = AF_INET;
-//            memcpy((void *) &request.sin_addr, hostp->h_addr, hostp->h_length);
-//            request.sin_port = htons((u_short)atoi(port));
-//            
-//            if (connect(request_sock, (struct sockaddr *)&request, sizeof (request)) < 0) {
-//                (void) close(request_sock);
-//                perror("connect");
-//                exit(1);
-//            }
-//            
-//            
-//            FD_SET(request_sock, &mask);			// listen request socket
-//            
-//            
-//            memset(buf, 0x00, sizeof buf);
-//            strncpy(buf, "GET ", strlen("GET "));
-//            strcat(buf, file);
-//            strcat(buf, " HTTP/1.0\nHOST: ");
-//            strcat(buf, ip);
-//            strcat(buf, "\nUser-agent: HW2/1.0");
-//            strcat(buf, "\nConnection: close\n");
-//            
-//            if (write(request_sock, buf, strlen(buf)) < 0) {
-//                perror("write");
-//                exit(1);
-//            }
-//        }// inner else
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        
-//        
-//        if(1) {
-//            int i, j;
-//            
-//            // Input from the keyboard
-//            memset(command, 0x00, sizeof command);
-//            
-//            addrlen = sizeof server;
-//            new_sock = accept(server_sock,
-//                              (struct sockaddr *)&server,
-//                              (socklen_t *)&addrlen);
-//            
-//            if (new_sock < 0) {
-//                perror("accept");
-//                exit(1);
-//            }
-//            
-//            printf("connection : Host IP %s, Port %d, socket %d\n",
-//                   inet_ntoa(server.sin_addr), ntohs(server.sin_port), new_sock);
-//            
-//            read(new_sock, command, sizeof command);	// read user command
-//            printf("%s", command);
-//            
-//            
-//            
-//            
-//            if(!strncmp(command, "quit", 4)){			// quit
-//                close(server_sock);
-//                return 0;
-//            }
-//            else if(!strncmp(command, "exit", 4)){		// exit
-//                close(server_sock);
-//                return 0;
-//            }
-//            else if(!strncmp(command, "connect", 7)){	// connect
-//                userInfo user = {inet_ntoa(server.sin_addr), &command[8], 1, 1};
-//                user_list.push_back(user);
-//                if (write(new_sock, "connecting success!", strlen("connecting success!")) < 0) {
-//                    perror("write");
-//                    exit(1);
-//                }
-//            }
-//            else if(!strncmp(command, "list", 4)){		// user & room list
-//                for(int i=0; i<user_list.size(); i++){
-//                    userInfo user = user_list.front();
-//                    printf("ID : %s, IP : %s, Port# : %d", user.id, user.ip, user.port); 			// need to fix
-//                }
-//            }
-//            else if(!strncmp(command, "invite", 6)){	// exit
-//                close(server_sock);
-//                return 0;
-//            }
-//            else if(!strncmp(command, "mkroom", 6)){	// mkroom
-//                room_size++;
-//            }
-//            else{										// error
-//                
-//            }
-//            
-//            
-//            
-//            if (write(new_sock, "connecting success!", strlen("connecting success!")) < 0) {
-//                perror("write");
-//                exit(1);
-//            }
-//            
-//            
-//            
-//            
-//            
-//            
-//            
-//            
-//            
-//            
-//            
-//            else {										// download
-//                int i, j;
-//                
-//                for(i=9; command[i] != ' '; i++)	// 9 = initial ip location
-//                    ip[i-9] = command[i];
-//                
-//                for(j=0; command[++i] != ' '; j++)	// port
-//                    port[j] = command[i];
-//                
-//                for(j=0; command[++i] != '\n'; j++)	// file
-//                    file[j] = command[i];
-//                
-//                
-//                if ((request_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-//                    perror("socket");
-//                    exit(1);
-//                }
-//                
-//                if ((hostp = gethostbyname(ip)) == 0) {
-//                    fprintf(stderr,"%s: unknown host\n", ip);
-//                    exit(1);
-//                }
-//                
-//                
-//                memset((void *) &request, 0, sizeof request);
-//                request.sin_family = AF_INET;
-//                memcpy((void *) &request.sin_addr, hostp->h_addr, hostp->h_length);
-//                request.sin_port = htons((u_short)atoi(port));
-//                
-//                if (connect(request_sock, (struct sockaddr *)&request, sizeof (request)) < 0) {
-//                    (void) close(request_sock);
-//                    perror("connect");
-//                    exit(1);
-//                }
-//                
-//                
-//                FD_SET(request_sock, &mask);			// listen request socket
-//                
-//                
-//                memset(buf, 0x00, sizeof buf);
-//                strncpy(buf, "GET ", strlen("GET "));
-//                strcat(buf, file);
-//                strcat(buf, " HTTP/1.0\nHOST: ");
-//                strcat(buf, ip);
-//                strcat(buf, "\nUser-agent: HW2/1.0");
-//                strcat(buf, "\nConnection: close\n");
-//                
-//                if (write(request_sock, buf, strlen(buf)) < 0) {
-//                    perror("write");
-//                    exit(1);
-//                }
-//            }// inner else
-//        }
-
-        
-        
-//        else if(FD_ISSET(server_sock, &rmask)){
-//            int i, j;
-//            
-//            addrlen = sizeof server;
-//            new_sock = accept(server_sock,
-//                              (struct sockaddr *)&server,
-//                              (socklen_t *)&addrlen);
-//            
-//            if (new_sock < 0) {
-//                perror("accept");
-//                exit(1);
-//            }
-//            
-//            printf("connection : Host IP %s, Port %d, socket %d\n",
-//                   inet_ntoa(server.sin_addr), ntohs(server.sin_port), new_sock);
-//            
-//            read(new_sock, buf, sizeof buf);
-//            printf("%s", buf);
-//            
-//            memset(file, 0x00, sizeof file);
-//            for(i=0; buf[i++] != '/';);
-//            for(j=0; buf[i] != ' '; i++)
-//                file[j++] = buf[i];
-//            
-//            memset(buf, 0x00, sizeof buf);	// reuse
-//            strncpy(buf, "HTTP/1.0 ", strlen("HTTP/1.0 "));
-//            
-//            strncpy(path, "./", strlen("./"));
-//            strcat(path, file);
-//            
-//            FILE* inf = fopen(path, "r");
-//            if (inf == NULL)
-//                strcat(buf, "404 NOT FOUND");
-//            else
-//                strcat(buf, "200 OK");
-//            
-//            strcat(buf, "\nConnection: close");
-//            if(inf != NULL){
-//                memset(html, 0x00, sizeof html);
-//                while(fscanf(inf, "%c", tmp) != EOF)
-//                    strcat(html, tmp);
-//            }
-//            
-//            sprintf(buf, "%s\nContent-Length: %ld", buf, strlen(html));
-//            strcat(buf, "\nContent-Type: text/html\n");
-//            
-//            if (inf == NULL){
-//                puts("p5");
-//                if (write(new_sock, buf, strlen(buf)) < 0) {
-//                    perror("write");
-//                    exit(1);
-//                }
-//                printf("Server Error: No such file ./%s\n", file);
-//            }
-//            else{
-//                strcat(buf, html);
-//                if (write(new_sock, buf, strlen(buf)) < 0) {
-//                    perror("write");
-//                    exit(1);
-//                }
-//                printf("finish: %lu %lu\n", strlen(html), strlen(html));
-//            }
-//        }
-//        
-//        
-//        else if(FD_ISSET(request_sock, &rmask)){
-//            memset(buf, 0x00, sizeof buf);
-//            read(request_sock, buf, sizeof buf);
-//            printf("%s\n", buf);
-//            
-//            close(request_sock);
-//            FD_ZERO(&mask);
-//            FD_SET(fileno(stdin), &mask);
-//            FD_SET(server_sock, &mask);
-//        }
-        
-    
